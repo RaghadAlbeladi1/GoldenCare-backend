65c74b807e486e183bc38544ba5ee8711c0e2a24
RaghadAlbeladi1 <Raghad.Z.albeladi@outlook.com>
2025-10-29 01:00:02 +0300

    add files

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render #besure for this if deleted
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+# Define the home view
+class Home(APIView):
+  def get(self, request):
+    content = {'message': 'Welcome to the GoldenCare API home route!'}
+    return Response(content)
