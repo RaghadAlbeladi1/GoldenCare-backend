dfe00618f4789c70e2d28543688fb6e5fec68711
RaghadAlbeladi1 <Raghad.Z.albeladi@outlook.com>
2025-11-01 01:00:05 +0300

    fix data

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-# Create your models here.
+img_placeholder="https://img.freepik.com/premium-vector/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-vector-illustration_561158-3407.jpg"
 
 
 class Service(models.Model):
@@ -9,7 +8,7 @@ class Service(models.Model):
     description = models.TextField(max_length=500)
     price_per_hour = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
     duration_options = models.CharField(max_length=100, default="1day,1month,3months")
-    image = models.URLField(blank=True)
+    imageURL = models.TextField(max_length=100, default=img_placeholder)
     
     def __str__(self):
         return self.service_name
@@ -19,12 +18,8 @@ class Service(models.Model):
 class Caregiver(models.Model):
     name = models.CharField(max_length=100)
     speciality = models.CharField(max_length=100)
-    bio = models.TextField(max_length=500, blank=True)
-    phone = models.CharField(max_length=20, blank=True)
-    email = models.EmailField(blank=True)
-    rating = models.DecimalField(max_digits=3, decimal_places=2, default=0.00)
-    certifications = models.TextField(max_length=300, blank=True)
-    image = models.URLField(blank=True)
+    bio = models.TextField(max_length=500)
+    imageURL = models.TextField(max_length=100, default=img_placeholder)
     services = models.ManyToManyField(Service, related_name='caregivers')
     
     def __str__(self):
